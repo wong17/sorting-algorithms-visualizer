347bcf6513a9aa0ad8548f96ff2db6d4520c5fc7
Wong <josuewong797@gmail.com>
2024-07-18 19:39:08 -0600

    Se elimina toolbar y se reemplaza por un header

--- sorting-algorithms-visualizer/src/app/app.component.ts
+++ sorting-algorithms-visualizer/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolBarComponent } from "../components/tool-bar/tool-bar.component";
 import { AlgorithmsVisualizerComponent } from "../components/algorithms-visualizer/algorithms-visualizer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolBarComponent, AlgorithmsVisualizerComponent],
+  imports: [RouterOutlet, AlgorithmsVisualizerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
