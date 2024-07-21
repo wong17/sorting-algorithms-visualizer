ea07390b1511616ebe1e64a84ccef28e18486029
Wong <josuewong797@gmail.com>
2024-07-20 18:07:51 -0600

    Se agrega algoritmo SelectionSort

--- sorting-algorithms-visualizer/src/algorithms/selection-sort.ts
+++ sorting-algorithms-visualizer/src/algorithms/selection-sort.ts
@@ -1,7 +1,46 @@
 import { SortingAlgorithm } from "./sorting-algorithm";
 
 export class SelectionSort extends SortingAlgorithm {
+
+  /**
+   * Inicia el proceso de ordenamiento usando el algoritmo de Selection Sort.
+   * Reinicia el historial de pasos y llama a la función selectionSort.
+   * 
+   * @param array - El arreglo a ordenar.
+   * @returns - El arreglo ordenado.
+   */
   override sort(array: number[]): number[] {
-    throw new Error("Method not implemented.");
+    // Reiniciar el paso a paso del algoritmo
+    this.steps = [];
+    return this.selectionSort(array, array.length);
+  }
+
+  /**
+   * Ordena el arreglo usando el algoritmo de Selection Sort.
+   * 
+   * @param array - El arreglo a ordenar.
+   * @param n - La longitud del arreglo.
+   * @returns - El arreglo ordenado.
+   */
+  private selectionSort(array: number[], n: number): number[] {
+    // Iterar sobre cada elemento del arreglo
+    for (let i = 0; i < n; i++) {
+      let min = i; // Suponer que el elemento actual es el mínimo
+
+      // Encontrar el elemento más pequeño en el subarreglo restante
+      for (let j = i + 1; j < n; j++) {
+        // Registrar el estado actual del arreglo y los índices comparados
+        this.steps.push([[...array], [min, j], []]);
+        if (array[j] < array[min]) {
+          min = j;
+        }
+      }
+      // Intercambiar el elemento actual con el elemento mínimo encontrado
+      [array[i], array[min]] = [array[min], array[i]];
+      // Registrar el estado del arreglo después del intercambio
+      this.steps.push([[...array], [], [i, min]]);
+    }
+
+    return array;
   }
 }
